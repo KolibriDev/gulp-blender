46b5c66e8fd55ebbf6d0f9cf2759df6a0142dab4
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-02-08 20:55:53 +0000

    Implement gulp-load-plugins (resolves #1) and gulp-connect (resolves #2) + minor update to README (getting started instructions) and package.json + version bumb 0.1.0

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This is our attempt at an even more badass customized front-end development workflow using **gulp**  for task automation. This will eventually be a project seed that you can use to build your own projects on.
 
 *Stay tuned*
+
+## Getting started
+
+Just some basics to setting this up and start the whole thing
+
+```shell
+$ git init your-project-name
+$ cd your-project-name
+$ git pull https://github.com/Form5/gulp-blender.git
+$ npm install
+$ npm start
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,58 @@
-var gulp        = require('gulp'),
-    gutil       = require('gulp-util'),
-    sass        = require('gulp-sass'),
-    csso        = require('gulp-csso'),
-    uglify      = require('gulp-uglify'),
-    jade        = require('gulp-jade'),
-    concat      = require('gulp-concat'),
-    livereload  = require('gulp-livereload'), // Livereload plugin needed: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
-    tinylr      = require('tiny-lr'),
-    express     = require('express'),
-    app         = express(),
-    path        = require('path'),
-    server      = tinylr(),
-    clean       = require('gulp-clean');
 
+var gulp    = require('gulp'),
+    plugins = require('gulp-load-plugins')();
 
-// --- Basic Tasks ---
+gulp.task('connect', plugins.connect.server({
+  root: __dirname + '/dev',
+  port: 1337,
+  livereload: true,
+  open: {
+    file: 'index.html',
+    browser: 'Google Chrome'
+  }
+}));
+
 gulp.task('css', function() {
   return gulp.src('src/scss/*.scss')
     .pipe(
-      sass({
+      plugins.sass({
       includePaths: ['src/scss'],
       errLogToConsole: true
     })
     )
-    .pipe( csso() )
+    .pipe( plugins.csso() )
     .pipe( gulp.dest('dev/css/') )
-    .pipe( livereload( server ));
+    .pipe( plugins.connect.reload() );
 });
 
 gulp.task('js', function() {
   return gulp.src('src/js/*.js')
-    .pipe( uglify() )
-    .pipe( concat('all.min.js'))
-    .pipe( gulp.dest('dev/js/'))
-    .pipe( livereload( server ));
+    .pipe( plugins.uglify() )
+    .pipe( gulp.dest('dev/js/') )
+    .pipe( plugins.connect.reload() );
 });
 
 gulp.task('templates', function() {
   return gulp.src('src/views/*.jade')
-    .pipe(jade({
+    .pipe(plugins.jade({
       pretty: true
     }))
     .pipe(gulp.dest('dev/'))
-    .pipe( livereload( server ));
+    .pipe( plugins.connect.reload() );
 });
 
-gulp.task('express', function() {
-  app.use(express.static(path.resolve('./dev')));
-  app.listen(1337);
-  gutil.log('Listening on port: 1337');
-});
-
+// TODO: Find a way to implement clean back into 'default' task;
+//       throwing errors like crazy atm!
 gulp.task('clean', function() {
-  gulp.src('dev', {read: false})
-    .pipe(clean());
+  gulp.src(['dev'], {read: false})
+    .pipe( plugins.clean() );
 });
 
 gulp.task('watch', function () {
-  server.listen(35729, function (err) {
-    if (err) {
-      return console.log(err);
-    }
-
-    gulp.watch('src/scss/*.scss',['css']);
-
-    gulp.watch('src/js/*.js',['js']);
-
-    gulp.watch('src/views/*.jade',['templates']);
-
-  });
+  gulp.watch('src/scss/*.scss',['css']);
+  gulp.watch('src/js/*.js',['js']);
+  gulp.watch('src/views/*.jade',['templates']);
 });
 
 // Default Task
-gulp.task('default', ['clean','js','css','templates','express','watch']);
+gulp.task('default', ['js', 'css', 'templates', 'connect', 'watch']);
