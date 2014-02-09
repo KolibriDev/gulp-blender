ccfaf373e7ce57806bb7ec3c8d72d62164b55d5f
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-02-09 05:14:24 +0000

    Implement gulp-jshint (resolves #4), implement gulp-notify (resolves #6), although error reporting is problematic (gulp.watch issues, see #12)

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,16 +1,34 @@
 var gulp    = require('gulp'),
+    // notify  = require('gulp-notify'),
+    jshint  = require('gulp-jshint'),
     cache   = require('gulp-cached'),
     uglify  = require('gulp-uglify'),
     connect = require('gulp-connect');
 
 module.exports = {
   'scripts': {
+    deps: ['script-lint'],
     callback: function() {
       return gulp.src('src/js/**/*.js')
         .pipe( cache('scripts') )
         .pipe( uglify() )
         .pipe( gulp.dest('dev/js/') )
         .pipe( connect.reload() );
+    }
+  },
+  'script-lint': {
+    callback: function() {
+      return gulp.src('src/js/**/*.js')
+        .pipe( cache('script-lint') )
+        .pipe( jshint('./.jshintrc') )
+        .pipe( jshint.reporter('default') )
+        // TODO: Figure out a way to stop this task on jshint failure,
+        //        without stopping the whole 'watch' task
+        // .pipe( jshint.reporter('fail') )
+        // .on('error', notify.onError(function(error) {
+        //   return 'jshint failed ' + error.message;
+        // }))
+        ;
     }
   }
 };
