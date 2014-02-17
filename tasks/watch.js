cdee2ea10cf0ffe52f4fa2e9b9e240fba804e736
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-02-17 23:17:09 +0000

    Make sure to only run -dev tasks on watch

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,11 +3,11 @@ var gulp = require('gulp');
 module.exports = {
   'watch': {
     callback: function() {
-      gulp.watch('src/scss/**/*.scss',['styles']);
-      gulp.watch('src/js/**/*.js',['scripts']);
-      gulp.watch('src/views/**/*.jade',['templates']);
-      gulp.watch('src/img/**/*.{png,gif,jpg,jpeg}',['images']);
-      gulp.watch('src/img/**/*.{svg}',['vectors']);
+      gulp.watch('src/scss/**/*.scss',['styles-dev']);
+      gulp.watch('src/js/**/*.js',['scripts-dev']);
+      gulp.watch('src/views/**/*.jade',['templates-dev']);
+      gulp.watch('src/img/**/*.{png,gif,jpg,jpeg}',['images-dev']);
+      gulp.watch('src/img/**/*.{svg}',['vectors-dev']);
     }
   }
 };
