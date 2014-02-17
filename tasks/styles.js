f0e69ed7938af5e2f1d937da108cc67fd067a385
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-02-17 23:16:32 +0000

    Disable caching on styles, since it was disabling reload when imported files are changed.

--- tasks/styles.js
+++ tasks/styles.js
@@ -5,11 +5,9 @@ var gulp    = require('gulp'),
 module.exports = {
   'styles-dev': {
     callback: function() {
-      var cache   = require('gulp-cached'),
-          connect = require('gulp-connect');
+      var connect = require('gulp-connect');
       return gulp.src('src/scss/*.scss')
-        .pipe( cache('styles-dev') )
-        .pipe( sass() )
+        .pipe( sass({noCache:true}) )
         .pipe( prefix('> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1') )
         .pipe( gulp.dest('dev/css/') )
         .pipe( connect.reload() );
@@ -19,7 +17,7 @@ module.exports = {
     callback: function() {
       var csso = require('gulp-csso');
       return gulp.src('src/scss/*.scss')
-        .pipe( sass() )
+        .pipe( sass({noCache:true}) )
         .pipe( prefix('> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1') )
         .pipe( csso() )
         .pipe( gulp.dest('dist/css/') );
