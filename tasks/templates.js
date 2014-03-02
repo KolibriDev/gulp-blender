3571f796a6926170fb903e9632e953a7f4d9b6d8
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-03-02 04:03:57 +0000

    Implement error handling for jade compiler.

--- tasks/templates.js
+++ tasks/templates.js
@@ -8,6 +8,9 @@ module.exports = function(gulp) {
     return gulp.src('./src/views/*.jade')
       .pipe( gulp.plugin.plumber() )
       .pipe( gulp.plugin.jade({pretty: !prod}) )
+      .on('error', gulp.plugin.notify.onError(function(error){
+        return error.message.split('\n').pop();
+      }))
       .pipe( gulp.dest(prod ? './dist/' : './dev/') )
       .pipe( !prod ? gutil.noop() : gulp.plugin.size() )
       .pipe( prod ? gutil.noop() : gulp.plugin.connect.reload() );
