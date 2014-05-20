33adf2f60ee04d70d07a3985e6f7cc625803ff5c
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-05-20 15:38:09 +0000

    Update all gulp packages to latest versions. Fix connect task due to breaking changes in gulp-connect, implementing individual open package

--- tasks/connect.js
+++ tasks/connect.js
@@ -2,13 +2,12 @@
 module.exports = function(gulp) {
   var prod = gulp.plugin.util.env.prod;
 
-  gulp.task('connect', ['build-dev'], gulp.plugin.connect.server({
-    root: [(prod ? './dist' : './dev')],
-    port: (prod ? 8000 : 1337),
-    livereload: true,
-    open: {
-      browser: 'Google Chrome'
-    }
-  }));
+  gulp.task('connect', function() {
+    return gulp.plugin.connect.server({
+      root: [(prod ? './dist' : './dev')],
+      port: 1337,
+      livereload: true
+    });
+  });
 
 };
--- tasks/server.js
+++ tasks/server.js
@@ -1,8 +1,15 @@
 
 module.exports = function(gulp) {
+  var runningServer = false;
 
   gulp.task('server', ['build-dev'], function() {
-    gulp.start('connect', 'watch');
+    runningServer = true;
+    gulp.start('connect','watch');
+  }).on('stop',function(e){
+    // TODO: Find more elegant solution
+    if (!runningServer) { return; }
+    runningServer = false;
+    require('open')('http://localhost:1337');
   });
 
 };
