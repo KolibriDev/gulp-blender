4c1320ab5766fe5c96dc9d5ceba7c4c06be84c49
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-05-20 15:11:05 +0000

    Include images and regular css files (renamed to scss) in bower task

--- tasks/bower.js
+++ tasks/bower.js
@@ -11,6 +11,8 @@ module.exports = function(gulp) {
       .on('end', function() {
 
         var jsFilter = gulp.plugin.filter(['**/*.js','**/*.map']),
+            imgFilter = gulp.plugin.filter('**/*.{png,gif,jpg,jpeg,svg}'),
+            cssFilter = gulp.plugin.filter('**/*.css'),
             scssFilter = gulp.plugin.filter('**/*.scss');
 
         return gulp.plugin.bowerFiles()
@@ -18,8 +20,15 @@ module.exports = function(gulp) {
 
           .pipe( jsFilter )
           .pipe( gulp.dest('./src/js/vendor/') )
-
           .pipe( jsFilter.restore() )
+
+          .pipe( imgFilter )
+          .pipe( gulp.dest('./src/img/vendor/') )
+          .pipe( imgFilter.restore() )
+
+          .pipe( cssFilter )
+          .pipe( gulp.plugin.rename({extname: '.scss'}) )
+          .pipe( cssFilter.restore() )
 
           .pipe( scssFilter )
           .pipe( gulp.dest('./src/scss/vendor/') );
