3c608bb854ca9dbf03b4f24d2e67e5fe961214db
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-05-20 15:35:28 +0000

    Inject data to jade files

--- tasks/templates.js
+++ tasks/templates.js
@@ -3,11 +3,15 @@ module.exports = function(gulp) {
 
   gulp.task('templates', function() {
     var gutil = gulp.plugin.util,
-        prod  = gutil.env.prod;
+        prod  = gutil.env.prod,
+        data = {
+          'env': prod ? 'production' : 'development',
+          'flags': gutil.env
+        };
 
     return gulp.src('./src/views/*.jade')
       .pipe( gulp.plugin.plumber() )
-      .pipe( gulp.plugin.jade({pretty: !prod}) )
+      .pipe( gulp.plugin.jade({pretty: !prod, data: data}) )
       .on('error', gulp.plugin.notify.onError(function(error){
         return error.message.split('\n').pop();
       }))
