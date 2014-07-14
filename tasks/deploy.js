6165d8d521e0a4721191208d2eae6913283a673f
Ólafur Örn Nielsen <olafur@form5.is>
2014-07-14 13:52:41 +0000

    Using promises for the deploy task. Fixes #25.

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -2,22 +2,32 @@
 
 module.exports = function(gulp) {
   var spawn = require('child_process').spawn,
-      pkg  = require('../package.json');
+      pkg  = require('../package.json'),
+      Q = require('q');
 
   if (!pkg.hasOwnProperty('blenderCmd')) {
+    console.error('blenderCmd object is missing from configuration.');
     return;
   }
   var cmd = pkg.blenderCmd;
 
   gulp.task('deploy', function() {
+    var deferred = Q.defer();
+
     if (cmd.hasOwnProperty('deploy') && cmd.deploy !== '') {
       var deploy = spawn(cmd.deploy);
       deploy.stdout.on('data', function(data) {
         process.stdout.write(data);
       });
+
+      deploy.stdout.on('close', function(){
+        return deferred.resolve();
+      });
     } else {
       console.log('Deployment command is missing! Add blenderCmd.deploy to package.json to use this task!');
+      return deferred.resolve();
     }
-  });
 
+    return deferred.promise;
+  });
 };
