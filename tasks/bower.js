d375a4eb242459963ad5cb2f0c13704d6f2de456
Ólafur Örn Nielsen <olafur@form5.is>
2014-07-14 14:23:55 +0000

    Updating the bower task, now using the new main-bower-files package.

--- tasks/bower.js
+++ tasks/bower.js
@@ -2,41 +2,27 @@
 module.exports = function(gulp) {
 
   gulp.task('bower', function() {
-    var bower = require('bower');
-    gulp.waitingForBower = false;
+    var mainBowerFiles = require('main-bower-files');
 
-    return bower.commands.install()
-      .on('error',gulp.plugin.notify.onError(function(error){
-          return error.data.endpoint.name + ' » ' + error.details;
-        }))
-      .on('end', function() {
+    var jsFilter = gulp.plugin.filter(['**/*.js','**/*.map']),
+        imgFilter = gulp.plugin.filter('**/*.{png,gif,jpg,jpeg,svg}'),
+        cssFilter = gulp.plugin.filter('**/*.css'),
+        scssFilter = gulp.plugin.filter('**/*.scss');
 
-        var jsFilter = gulp.plugin.filter(['**/*.js','**/*.map']),
-            imgFilter = gulp.plugin.filter('**/*.{png,gif,jpg,jpeg,svg}'),
-            cssFilter = gulp.plugin.filter('**/*.css'),
-            scssFilter = gulp.plugin.filter('**/*.scss');
+    return gulp.src(mainBowerFiles())
+      .pipe( jsFilter )
+      .pipe( gulp.dest('./src/js/vendor/') )
+      .pipe( jsFilter.restore() )
 
-        gulp.waitingForBower = true;
+      .pipe( imgFilter )
+      .pipe( gulp.dest('./src/img/vendor/') )
+      .pipe( imgFilter.restore() )
 
-        return gulp.plugin.bowerFiles()
-          .pipe( gulp.plugin.rename({dirname: ''}) )
+      .pipe( cssFilter )
+      .pipe( gulp.plugin.rename({extname: '.scss'}) )
+      .pipe( cssFilter.restore() )
 
-          .pipe( jsFilter )
-          .pipe( gulp.dest('./src/js/vendor/') )
-          .pipe( jsFilter.restore() )
-
-          .pipe( imgFilter )
-          .pipe( gulp.dest('./src/img/vendor/') )
-          .pipe( imgFilter.restore() )
-
-          .pipe( cssFilter )
-          .pipe( gulp.plugin.rename({extname: '.scss'}) )
-          .pipe( cssFilter.restore() )
-
-          .pipe( scssFilter )
-          .pipe( gulp.dest('./src/scss/vendor/') );
-      });
-
+      .pipe( scssFilter )
+      .pipe( gulp.dest('./src/scss/vendor/') );
   });
-
 };
