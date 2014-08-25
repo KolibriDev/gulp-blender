2c711637ace8fba88ba03f312bbe6dab4f81b989
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-08-25 09:23:23 +0000

    Fix versioning in package.json, causing errors for npm + bump

--- tasks/bump.js
+++ tasks/bump.js
@@ -6,7 +6,7 @@ module.exports = function(gulp) {
     var bumpType = 'patch';
     bumpType = gulp.plugin.util.env.minor ? 'minor' : bumpType;
     bumpType = gulp.plugin.util.env.major ? 'major' : bumpType;
-    gulp.src('./*.json')
+    gulp.src('./package.json','./bower.json')
       .pipe( gulp.plugin.plumber() )
       .pipe( gulp.plugin.bump({type:bumpType}) )
       .pipe( gulp.dest('./') );
