2b95802739a0364068e75a126277ae557930b677
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-08-25 09:20:22 +0000

    Fix scripts filter for bower task, adjust other filters to match format

--- tasks/bower.js
+++ tasks/bower.js
@@ -4,10 +4,10 @@ module.exports = function(gulp) {
   var mainBowerFiles = require('main-bower-files');
 
   var bowerFiles = function() {
-    var jsFilter = gulp.plugin.filter(gulp.cfg.scripts.src),
-        imgFilter = gulp.plugin.filter(gulp.cfg.images.src),
-        cssFilter = gulp.plugin.filter(gulp.cfg.bower.cssFilter),
-        scssFilter = gulp.plugin.filter(gulp.cfg.bower.scssFilter);
+    var jsFilter = gulp.plugin.filter(gulp.cfg.scripts.jsFilter),
+        imgFilter = gulp.plugin.filter(gulp.cfg.images.imgFilter),
+        cssFilter = gulp.plugin.filter(gulp.cfg.styles.cssFilter),
+        scssFilter = gulp.plugin.filter(gulp.cfg.styles.scssFilter);
 
     return gulp.src(mainBowerFiles())
       .pipe( jsFilter )
