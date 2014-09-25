44a601705dd64e3bfbbc89105b231a31a546f27e
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-09-25 23:59:05 +0000

    Refactor watch task to use blender.json values

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,10 +2,10 @@
 
 module.exports = function(gulp) {
   gulp.task('watch', ['connect'], function() {
-    gulp.watch('./src/scss/**/*.scss',['styles']);
-    gulp.watch('./src/js/**/*.{js,map}',['scripts']);
-    gulp.watch('./src/views/**/*.jade',['templates']);
-    gulp.watch(['./src/img/**/*.ico','./src/fonts/**/*','./src/videos/**/*'],['copy']);
-    gulp.watch('./src/img/**/*.{png,gif,jpg,jpeg,svg}',['images']);
+    gulp.watch(gulp.cfg.styles.watchSrc,['styles']);
+    gulp.watch(gulp.cfg.scripts.src,['scripts']);
+    gulp.watch(gulp.cfg.templates.watchSrc,['templates']);
+    gulp.watch([gulp.cfg.ico.src,gulp.cfg.fonts.src,gulp.cfg.videos.src],['copy']);
+    gulp.watch(gulp.cfg.images.src,['images']);
   });
 };
