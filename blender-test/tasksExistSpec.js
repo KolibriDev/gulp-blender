d7498d0442e09ca4e393682f207c1f05bcbf02c5
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-06-17 04:06:40 +0000

    Fix tasksExistSpec to match new tasks

--- blender-test/tasksExistSpec.js
+++ blender-test/tasksExistSpec.js
@@ -14,56 +14,53 @@
   });
 
   describe('Task runner', function() {
-    it('should have a build task ', function() {
-      expect(gulp.hasTask('build')).toBe(true);
-    });
-
-    it('should have a rimraf task ', function() {
-      expect(gulp.hasTask('rimraf')).toBe(true);
-    });
-
     it('should have a bower task ', function() {
       expect(gulp.hasTask('bower')).toBe(true);
     });
-
+    it('should have a build task ', function() {
+      expect(gulp.hasTask('build')).toBe(true);
+    });
+    it('should have a bump task ', function() {
+      expect(gulp.hasTask('bump')).toBe(true);
+    });
+    it('should have a clean task ', function() {
+      expect(gulp.hasTask('clean')).toBe(true);
+    });
+    it('should have a connect task ', function() {
+      expect(gulp.hasTask('connect')).toBe(true);
+    });
+    it('should have a copy task ', function() {
+      expect(gulp.hasTask('copy')).toBe(true);
+    });
+    it('should have a cress task ', function() {
+      expect(gulp.hasTask('cress')).toBe(true);
+    });
+    it('should have a default task ', function() {
+      expect(gulp.hasTask('default')).toBe(true);
+    });
+    it('should have a images task ', function() {
+      expect(gulp.hasTask('images')).toBe(true);
+    });
+    it('should have a notify task ', function() {
+      expect(gulp.hasTask('notify')).toBe(true);
+    });
+    it('should have a run task ', function() {
+      expect(gulp.hasTask('run')).toBe(true);
+    });
     it('should have a scripts task ', function() {
       expect(gulp.hasTask('scripts')).toBe(true);
     });
-
-    it('should have a styles task', function() {
-      expect(gulp.hasTask('styles')).toBe(true);
-    });
-
-    it('should have a templates task', function() {
-      expect(gulp.hasTask('templates')).toBe(true);
-    });
-
-    it('should have an images task', function() {
-      expect(gulp.hasTask('images')).toBe(true);
-    });
-
-    it('should have a copy task', function() {
-      expect(gulp.hasTask('copy')).toBe(true);
-    });
-
-    it('should have a server task', function() {
+    it('should have a server task ', function() {
       expect(gulp.hasTask('server')).toBe(true);
     });
-
-    it('should have a connect task', function() {
-      expect(gulp.hasTask('connect')).toBe(true);
+    it('should have a styles task ', function() {
+      expect(gulp.hasTask('styles')).toBe(true);
     });
-
-    it('should have a watch task', function() {
+    it('should have a templates task ', function() {
+      expect(gulp.hasTask('templates')).toBe(true);
+    });
+    it('should have a watch task ', function() {
       expect(gulp.hasTask('watch')).toBe(true);
-    });
-
-    it('should have a bump task', function() {
-      expect(gulp.hasTask('bump')).toBe(true);
-    });
-
-    it('should have a cress task', function() {
-      expect(gulp.hasTask('cress')).toBe(true);
     });
   });
 }).call(this);
