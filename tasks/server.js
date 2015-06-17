5925c2e8dcae574297b12f45bf57382ae227df99
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-06-17 02:30:08 +0000

    Remove open functionality from server Seriously, this has proven to be a ridiculously annoying feature

--- tasks/server.js
+++ tasks/server.js
@@ -1,15 +1,7 @@
 'use strict';
 
 module.exports = function(gulp) {
-  var runningServer = false;
-
   gulp.task('server', ['build'], function() {
-    runningServer = true;
     gulp.start('connect','watch');
-  }).on('stop',function(){
-    // TODO: Find more elegant solution
-    if (!runningServer) { return; }
-    runningServer = false;
-    require('open')('http://localhost:1337');
   });
 };
