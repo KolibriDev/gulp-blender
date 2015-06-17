7829023aa62577135cba1b6abf3a8f34ab6b50da
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-06-17 02:21:51 +0000

    Use del instead of gulp-rimraf as recommended

---
+++ tasks/clean.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(gulp) {
+  var del = require('del');
+  gulp.task('clean', function() {
+    return del([gulp.cfg.env.dir + '/*']);
+  });
+};
--- tasks/rimraf.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(gulp) {
-  gulp.task('rimraf', function() {
-    return gulp.src(gulp.cfg.env.dir +'/*', {read: false})
-      .pipe( gulp.plugin.rimraf() );
-  });
-};
