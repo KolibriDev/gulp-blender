bf5303de0cdce3f271a045360b28f08a382633c0
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-06-17 03:48:56 +0000

    Tweak watch tasks Trigger template update on svg change, since many svgs are included into jade files directly. Also update copy watch

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,8 +4,8 @@ module.exports = function(gulp) {
   gulp.task('watch', ['connect'], function() {
     gulp.watch(gulp.cfg.styles.watchSrc,['styles']);
     gulp.watch(gulp.cfg.scripts.src,['scripts']);
-    gulp.watch(gulp.cfg.templates.watchSrc,['templates']);
-    gulp.watch([gulp.cfg.ico.src,gulp.cfg.fonts.src,gulp.cfg.videos.src],['copy']);
+    gulp.watch([gulp.cfg.templates.watchSrc,gulp.cfg.svg.src],['templates']);
+    gulp.watch([gulp.cfg.ico.src,gulp.cfg.svg.src,gulp.cfg.fonts.src,gulp.cfg.videos.src],['copy']);
     gulp.watch(gulp.cfg.images.src,['images']);
   });
 };
