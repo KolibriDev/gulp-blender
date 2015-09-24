daa6b00abedd81bf3926c7d9401fd73596105fb5
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-09-24 11:53:55 +0000

    Rewrite copy task with merged streams

--- tasks/copy.js
+++ tasks/copy.js
@@ -2,17 +2,25 @@
 
 module.exports = function(gulp) {
   var path = require('path');
+  var merge = require('merge-stream');
+
   gulp.task('copy', function() {
-    gulp.src(gulp.cfg.ico.src)
-      .pipe( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.ico.subDir)) );
+    var ico = gulp.src(gulp.cfg.ico.src)
+      .pipe ( gulp.plugin.debug({title:'--ico:'}) )
+      .pipe ( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.ico.subDir)) );
 
-    gulp.src(gulp.cfg.svg.src)
-      .pipe( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.svg.subDir)) );
+    var svg = gulp.src(gulp.cfg.svg.src)
+      .pipe ( gulp.plugin.debug({title:'--svg:'}) )
+      .pipe ( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.svg.subDir)) );
 
-    gulp.src(gulp.cfg.fonts.src)
-      .pipe( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.fonts.subDir)) );
+    var fonts = gulp.src(gulp.cfg.fonts.src)
+      .pipe ( gulp.plugin.debug({title:'--fonts:'}) )
+      .pipe ( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.fonts.subDir)) );
 
-    gulp.src(gulp.cfg.videos.src)
-      .pipe( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.videos.subDir)) );
+    var videos = gulp.src(gulp.cfg.videos.src)
+      .pipe ( gulp.plugin.debug({title:'--videos:'}) )
+      .pipe ( gulp.dest(path.join(gulp.cfg.envdir, gulp.cfg.videos.subDir)) );
+
+    return merge(ico, svg, fonts, videos);
   });
 };
