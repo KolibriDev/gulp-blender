253260fbc9407e77ccd9634463e6f59827fea86b
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-09-24 13:38:16 +0000

    Fix bower task with new gulp-filter functionality

--- tasks/bower.js
+++ tasks/bower.js
@@ -2,24 +2,24 @@
 
 module.exports = function(gulp) {
   var bowerFiles = require('main-bower-files');
-  var jsFilter = gulp.plugin.filter(gulp.cfg.scripts.jsFilter),
-      imgFilter = gulp.plugin.filter(gulp.cfg.images.imgFilter),
-      cssFilter = gulp.plugin.filter(gulp.cfg.styles.cssFilter),
-      scssFilter = gulp.plugin.filter(gulp.cfg.styles.scssFilter);
+  var jsFilter = gulp.plugin.filter(gulp.cfg.scripts.jsFilter, {restore: true}),
+      imgFilter = gulp.plugin.filter(gulp.cfg.images.imgFilter, {restore: true}),
+      cssFilter = gulp.plugin.filter(gulp.cfg.styles.cssFilter, {restore: true}),
+      scssFilter = gulp.plugin.filter(gulp.cfg.styles.scssFilter, {restore: true});
 
   gulp.task('bower', function() {
     return gulp.src(bowerFiles())
       .pipe ( jsFilter )
       .pipe ( gulp.dest(gulp.cfg.bower.jsDest) )
-      .pipe ( jsFilter.restore() )
+      .pipe ( jsFilter.restore )
 
       .pipe ( imgFilter )
       .pipe ( gulp.dest(gulp.cfg.bower.imgDest) )
-      .pipe ( imgFilter.restore() )
+      .pipe ( imgFilter.restore )
 
       .pipe ( cssFilter )
       .pipe ( gulp.plugin.rename({extname: '.scss'}) )
-      .pipe ( cssFilter.restore() )
+      .pipe ( cssFilter.restore )
 
       .pipe ( scssFilter )
       .pipe ( gulp.dest(gulp.cfg.bower.scssDest) );
