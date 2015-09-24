4846672742d1e549abe874657c477a680cb60fd0
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-09-24 12:00:24 +0000

    Update styles task
    Make sure to return stream

--- tasks/styles.js
+++ tasks/styles.js
@@ -2,12 +2,15 @@
 
 module.exports = function(gulp) {
   var path = require('path');
+  var noop = gulp.plugin.util.noop;
   var prod  = gulp.cfg.env === 'production';
   var dir = path.join(gulp.cfg.envdir, gulp.cfg.styles.subDir);
 
   gulp.task('styles', function() {
-    gulp.src(gulp.cfg.styles.src)
+    return gulp.src(gulp.cfg.styles.src)
       .pipe ( gulp.plugin.plumber({errorHandler: gulp.plugin.notify.onError('<%= error.message %>')}) )
+
+      .pipe ( prod ? noop() : gulp.plugin.debug({title:'--sass:'}) )
 
       .pipe ( gulp.plugin.sass({ outputStyle: (prod ? 'compressed' : 'nested') }) )
       .pipe ( gulp.plugin.autoprefixer(gulp.cfg.styles.autoprefixer) )
