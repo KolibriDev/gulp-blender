1e7d9482c63b8785263f49f78edd8c8fd3fdd87b
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-09-27 22:25:35 +0000

    Fix build-require & add requirejs to .jshintrc

--- build-require.js
+++ build-require.js
@@ -1,8 +1,9 @@
 ({
-  appDir: 'src/js',
-  baseUrl: './',
-  dir: 'dist/js/',
-  mainConfigFile: 'src/js/main.js',
+  allowSourceOverwrites: true,
+  keepBuildDir: true,
+  baseUrl: './dist/js',
+  dir: 'dist/js',
+  mainConfigFile: 'dist/js/main.js',
   optimize: 'uglify2', // "none": No minification, "uglify": Full minification
   modules: [
     {
